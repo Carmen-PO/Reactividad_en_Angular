1ffa79232ae3d8f05b3b91c7fbc30234be0c272e
Carmen-PO <carmenpinuelaotero@outlook.com>
2025-11-13 00:01:59 +0100

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { Componente1Component } from './componente1/componente1';
-import { Componente2Component } from './componente2/componente2';
-
-export const routes: Routes = [
-  { path: '', component: Componente1Component },
-  { path: 'componente2', component: Componente2Component }
-];
-
